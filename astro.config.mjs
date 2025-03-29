23a7e6569ffbeef4c7bee62673624e9d42e54abd
AksharDesai <akshar25desai@gmail,com>
2025-03-29 10:56:28 +0530

    added images and cofigured astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
+
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
   }
